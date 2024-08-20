79172b62f3e17648b794dea73d0c17b4274baf28
James Birtles <james@birtles.dev>
2024-08-20 19:55:04 +0100

    list some recipes

---
+++ src/lib/components/RecipeCard.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Card, CardHeader, CardTitle } from './ui/card';
+
+	export let recipe;
+</script>
+
+<Card class="overflow-clip">
+	<img
+		src="https://loremflickr.com/400/260/meal,dish?random={Math.random()}"
+		alt=""
+		width="400"
+		height="260"
+	/>
+	<CardHeader>
+		<CardTitle>{recipe.title}</CardTitle>
+	</CardHeader>
+</Card>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,30 @@
 <script lang="ts">
+	import RecipeCard from '$lib/components/RecipeCard.svelte';
+
 	export let data;
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="mx-auto max-w-screen-2xl p-6">
+	<h2
+		class="mb-8 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+	>
+		Saved Recipes
+	</h2>
 
-<pre>{JSON.stringify(data, null, 2)}</pre>
+	<div class="grid grid-cols-5 gap-4">
+		{#each data.recipes as recipe}
+			<RecipeCard {recipe} />
+			<RecipeCard {recipe} />
+			<RecipeCard {recipe} />
+			<RecipeCard {recipe} />
+			<RecipeCard {recipe} />
+			<RecipeCard {recipe} />
+			<RecipeCard {recipe} />
+			<RecipeCard {recipe} />
+			<RecipeCard {recipe} />
+			<RecipeCard {recipe} />
+		{/each}
+	</div>
+
+	<pre>{JSON.stringify(data, null, 2)}</pre>
+</div>
