3716559662591b22d1254a73900760a15fcb0920
James Birtles <james@birtles.dev>
2024-08-21 16:48:36 +0100

    add dev process

--- devenv.nix
+++ devenv.nix
@@ -3,4 +3,10 @@
 {
   languages.javascript.enable = true;
   languages.javascript.npm.enable = true;
+  processes.dev-server = {
+    exec = "npm run dev";
+    process-compose.environment = [
+      "FORCE_COLOR=1"
+    ];
+  };
 }
