8166436a956a9b5bb69d280037364826b6427cf5
James Birtles <james@birtles.dev>
2024-08-21 19:40:45 +0100

    decode html entities in recipe text

--- src/lib/StructuredData.ts
+++ src/lib/StructuredData.ts
@@ -1,25 +1,35 @@
 import { Schema } from '@effect/schema';
 
+/**
+ * Same as Schema.String except that some cleanup is performed such as converting
+ * some html entities back into their original character
+ */
+const String = Schema.transform(Schema.String, Schema.String, {
+	strict: true,
+	decode: (s) => s.replaceAll('&#39;', "'").replaceAll('&quot;', '"').replaceAll('&amp;', '&'),
+	encode: (s) => s,
+});
+
 export const HowToStep = Schema.Struct({
 	_tag: Schema.tag('HowToStep').pipe(Schema.fromKey('@type')),
-	name: Schema.String.pipe(Schema.optionalWith({ as: 'Option' })),
-	text: Schema.String,
+	name: String.pipe(Schema.optionalWith({ as: 'Option' })),
+	text: String,
 	// TODO: support itemListElement - https://developers.google.com/search/docs/appearance/structured-data/recipe#how-to-step
 });
 export const HowToSection = Schema.Struct({
 	_tag: Schema.tag('HowToSection').pipe(Schema.fromKey('@type')),
-	name: Schema.String.pipe(Schema.optionalWith({ as: 'Option' })),
+	name: String.pipe(Schema.optionalWith({ as: 'Option' })),
 	itemListElement: Schema.Array(HowToStep),
 });
 export const RecipeInstructions = Schema.Union(
 	Schema.ArrayEnsure(HowToStep),
 	Schema.ArrayEnsure(HowToSection),
-	Schema.ArrayEnsure(Schema.String),
+	Schema.ArrayEnsure(String),
 );
 
 export const Recipe = Schema.Struct({
 	'@type': Schema.tag('Recipe'),
-	name: Schema.String,
+	name: String,
 	recipeInstructions: RecipeInstructions.pipe(Schema.optionalWith({ as: 'Option' })),
 });
 export const decodeRecipe = Schema.decodeUnknownSync(Recipe);
