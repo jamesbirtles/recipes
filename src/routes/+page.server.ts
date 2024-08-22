2342bb67b3e8c4214ba1bd152b34ea97ad7a5e47
James Birtles <james@birtles.dev>
2024-08-22 17:56:59 +0100

    add pagination

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,13 +1,20 @@
 import { redirect } from '@sveltejs/kit';
 
+const pageSize = 20;
 export const load = async ({ url, locals: { supabase } }) => {
 	const query = url.searchParams.get('query')?.trim();
-	let dbQuery = supabase.from('recipes').select().order('created_at');
+	const page = Number(url.searchParams.get('page')) || 0;
+	let dbQuery = supabase
+		.from('recipes')
+		.select('*', { count: 'exact' })
+		.order('created_at')
+		.range(page * pageSize, (page + 1) * pageSize - 1);
 	if (query) {
 		dbQuery = dbQuery.textSearch('title', query, { type: 'websearch' });
 	}
 	const result = await dbQuery;
-	return { recipes: result.data ?? [], query };
+	console.log(result);
+	return { recipes: result.data ?? [], query, count: result.count!, pageSize, page };
 };
 
 export const actions = {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,21 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
 	import RecipeCard from '$lib/components/RecipeCard.svelte';
 	import { Button } from '$lib/components/ui/button';
 	import { Input } from '$lib/components/ui/input';
+	import {
+		Pagination,
+		PaginationContent,
+		PaginationItem,
+		PaginationPrevButton,
+		PaginationEllipsis,
+		PaginationLink,
+		PaginationNextButton,
+	} from '$lib/components/ui/pagination';
 	import { decodeRecipe } from '$lib/Recipe.js';
+	import { count } from 'effect/Schedule';
+	import { ChevronLeft, ChevronRight } from 'lucide-svelte';
 	import ImportIcon from 'lucide-svelte/icons/import';
 	import LogoutIcon from 'lucide-svelte/icons/log-out';
 
@@ -46,12 +59,58 @@
 	{/each}
 </div>
 
-<div class="mt-8 flex items-center justify-center">
-	<Button href="/import" class="[view-transition-name:import-button]">
-		<span class="flex items-center [view-transition-name:import-button-text]">
-			<ImportIcon class="mr-4 h-4 w-4" /> Import Recipe
-		</span>
-	</Button>
-</div>
+{#if data.count > data.pageSize}
+	<Pagination
+		class="mt-8"
+		count={data.count}
+		perPage={data.pageSize}
+		page={data.page + 1}
+		onPageChange={(newPage) => {
+			const url = new URL($page.url);
+			url.searchParams.set('page', (newPage - 1).toString());
+			goto(url, { replaceState: true });
+		}}
+		let:pages
+		let:currentPage
+	>
+		<PaginationContent>
+			<PaginationItem>
+				<PaginationPrevButton>
+					<ChevronLeft class="h-4 w-4" />
+					<span class="hidden sm:block">Previous</span>
+				</PaginationPrevButton>
+			</PaginationItem>
+			{#each pages as page (page.key)}
+				{#if page.type === 'ellipsis'}
+					<PaginationItem>
+						<PaginationEllipsis />
+					</PaginationItem>
+				{:else}
+					<PaginationItem>
+						<PaginationLink {page} isActive={currentPage === page.value}>
+							{page.value}
+						</PaginationLink>
+					</PaginationItem>
+				{/if}
+			{/each}
+			<PaginationItem>
+				<PaginationNextButton>
+					<span class="hidden sm:block">Next</span>
+					<ChevronRight class="h-4 w-4" />
+				</PaginationNextButton>
+			</PaginationItem>
+		</PaginationContent>
+	</Pagination>
+{/if}
+
+{#if !data.query}
+	<div class="mt-8 flex items-center justify-center">
+		<Button href="/import" class="[view-transition-name:import-button]">
+			<span class="flex items-center [view-transition-name:import-button-text]">
+				<ImportIcon class="mr-4 h-4 w-4" /> Import Recipe
+			</span>
+		</Button>
+	</div>
+{/if}
 
 <!-- <pre class="mt-48">{JSON.stringify(data.user, null, 2)}</pre> -->
