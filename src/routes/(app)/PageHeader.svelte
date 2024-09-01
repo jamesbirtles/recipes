2bcd7ded64863babb67efd74254d050162b9cafe
James Birtles <james@birtles.dev>
2024-09-01 13:56:59 +0100

    improve title a little on mobile

--- src/routes/(app)/PageHeader.svelte
+++ src/routes/(app)/PageHeader.svelte
@@ -6,9 +6,9 @@
 	export let title: string;
 </script>
 
-<div class="mb-8 flex h-16 items-center gap-4 border-b">
-	<h1 class="scroll-m-20 text-3xl font-semibold tracking-tight transition-colors">{title}</h1>
-	<form method="post" action="/?/logout" class="ml-auto">
+<div class="mb-8 flex flex-col items-center gap-4 border-b pb-4 sm:h-16 sm:flex-row sm:pb-0">
+	<h1 class="text-3xl font-semibold tracking-tight transition-colors">{title}</h1>
+	<form method="post" action="/?/logout" class="sm:ml-auto">
 		{$page.data.user?.email}
 		<Button type="submit" size="sm" variant="secondary" class="ml-4">
 			Logout
